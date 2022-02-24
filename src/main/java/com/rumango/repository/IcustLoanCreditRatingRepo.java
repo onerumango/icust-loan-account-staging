7d7ad87a9d7b6ad80192215d8c09148b1a37d8a9
PoojaRumango <pooja.lakshmana@rumango.com>
2022-02-24 14:51:06 +0530

    added repository IcustLoanCreditRatingRepo

---
+++ src/main/java/com/rumango/repository/IcustLoanCreditRatingRepo.java
@@ -0,0 +1,9 @@
+package com.rumango.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rumango.entity.IcustLoanCreditRatingDetails;
+
+public interface IcustLoanCreditRatingRepo extends JpaRepository<IcustLoanCreditRatingDetails, Long>{
+
+}
