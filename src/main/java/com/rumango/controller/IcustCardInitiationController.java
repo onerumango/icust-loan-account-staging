8dbcfe79d8387c25f0717ccb47ddc2833c67e663
vidyashreeAcharya <vidyashree.a@rumango.com>
2022-04-27 20:40:07 +0530

    added service for digital signing

--- src/main/java/com/rumango/controller/IcustCardInitiationController.java
+++ src/main/java/com/rumango/controller/IcustCardInitiationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,6 +50,19 @@ public class IcustCardInitiationController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
 		} finally {
 			logger.info("Execution completed for fetchCardInitiationByCardAccId");
+		}
+	}
+	
+	@PutMapping("/updateStatus")
+	public ResponseEntity<?> updateStatusApproveOrReject(@RequestBody IcustCardInitiationModel cardInitiationModel) {
+		logger.info("Exectution started for updateStatusApproveOrReject: " + cardInitiationModel.getCardId());
+		try {
+			return cardInitiationService.updateStatusApproveOrReject(cardInitiationModel);
+		} catch (Exception e) {
+			logger.error("Execption occoured while executing updateStatusApproveOrReject", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		} finally {
+			logger.info("Execution completed for updateStatusApproveOrReject");
 		}
 	}
 }
